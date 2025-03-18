e1226bfa0be8c0378460080f91f7ca11d3362dd9
Adithya Firmansyah Putra <aditputrafirmansyah@gmail.com>
2025-03-18 14:36:20 +0700

    initial commit

---
+++ TungTung/TungTungApp.swift
@@ -0,0 +1,17 @@
+//
+//  TungTungApp.swift
+//  TungTung
+//
+//  Created by Adithya Firmansyah Putra on 18/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TungTungApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
