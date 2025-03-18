30c6c31d8d001bf4a49fd59929514124a7cb8a04
Adithya Firmansyah Putra <aditputrafirmansyah@gmail.com>
2025-03-18 16:49:12 +0700

    feat: add patungan screen

---
+++ TungTung/AddPatunganView.swift
@@ -0,0 +1,237 @@
+import SwiftUI
+
+struct AddPatunganView: View {
+    @State private var title = ""
+    @State private var price: String = ""
+    @State private var members: [Member] = []
+    @State private var paymentOptions: [PaymentOption] = []
+    @State private var agreementRule = ""
+    
+    @State private var newMemberName = ""
+    @State private var newPaymentAccount = ""
+    @State private var newPaymentBank = ""
+    @State private var newPaymentOwner = ""
+    
+    @State private var showAddMemberSheet = false
+    @State private var showAddPaymentSheet = false
+    @State private var memberAmount: Double = 0.0
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Judul Patungan")) {
+                    TextField("Contoh: Nobar Interstellar", text: $title)
+                        .background(Color.clear)
+                        .cornerRadius(8)
+                }
+                
+                Section(header: Text("Jumlah Harga")) {
+                    HStack {
+                        Text("Rp")
+                        TextField("500.000", text: $price)
+                            .keyboardType(.numberPad)
+                            .onChange(of: price) { newValue in
+                                calculateMemberAmount()
+                            }
+                            .background(Color.clear)
+                            .cornerRadius(8)
+                    }
+                }
+
+                Section(header: Text("Tambah Orang")) {
+                    ForEach(members, id: \.name) { member in
+                        HStack {
+                            Text(member.name)
+                            Spacer()
+                            Text("Rp \(member.amount, specifier: "%.2f")") // Format the amount
+                        }
+                    }.onDelete(perform: deleteMember)
+                    Button("Tambah Orang") {
+                        // Show sheet to add a new member
+                        showAddMemberSheet.toggle()
+                    }
+                    .sheet(isPresented: $showAddMemberSheet) {
+                        AddMemberSheet(newMemberName: $newMemberName, onSave: {
+                            if !newMemberName.isEmpty {
+                                // Add new member to the list
+                                members.append(Member(name: newMemberName, amount: memberAmount))
+                                newMemberName = "" // Clear the input field
+                                calculateMemberAmount() // Recalculate amount per person
+                                showAddMemberSheet.toggle()
+                            }
+                        }, onCancel: {
+                            // Reset the new member name when canceled
+                            newMemberName = ""
+                            showAddMemberSheet.toggle()
+                        })
+                    }
+                }
+
+                Section(header: Text("Tambah Opsi Pembayaran")) {
+                    ForEach(paymentOptions, id: \.accountNumber) { option in
+                        HStack {
+                            Text(option.accountNumber)
+                            Spacer()
+                            Text(option.bankName)
+                            Spacer()
+                            Text(option.owner)
+                        }
+                    }.onDelete(perform: deletePaymentOption)
+                    Button("Tambah Opsi Pembayaran") {
+                        // Show sheet to add a new payment option
+                        showAddPaymentSheet.toggle()
+                    }
+                    .sheet(isPresented: $showAddPaymentSheet) {
+                        AddPaymentSheet(newPaymentAccount: $newPaymentAccount, newPaymentBank: $newPaymentBank, newPaymentOwner: $newPaymentOwner, onSave: {
+                            if !newPaymentAccount.isEmpty && !newPaymentBank.isEmpty && !newPaymentOwner.isEmpty {
+                                // Add new payment option to the list
+                                paymentOptions.append(PaymentOption(accountNumber: newPaymentAccount, bankName: newPaymentBank, owner: newPaymentOwner))
+                                newPaymentAccount = "" // Clear the input fields
+                                newPaymentBank = ""
+                                newPaymentOwner = ""
+                                showAddPaymentSheet.toggle()
+                            }
+                        }, onCancel: {
+                            
+                            newPaymentAccount = ""
+                            newPaymentBank = ""
+                            newPaymentOwner = ""
+                            showAddPaymentSheet.toggle()
+                        })
+                    }
+                }
+
+                Section(header: Text("Agreement Rule")) {
+                    TextEditor(text: $agreementRule)
+                        .background(Color.clear)
+                        .cornerRadius(8)
+                        .padding(.horizontal)
+                }
+                
+                Button("Simpan") {
+
+                }
+                
+                
+            }
+            .navigationTitle("Tambah Patungan")
+            .navigationBarBackButtonHidden(false)
+        }
+    }
+    
+    
+    func calculateMemberAmount() {
+        guard let totalPrice = Double(price), totalPrice > 0 else {
+            memberAmount = 0.0
+            for index in members.indices {
+                members[index].amount = 0.0
+            }
+            return
+        }
+        
+        let numberOfMembers = Double(members.count)
+        if numberOfMembers > 0 {
+            memberAmount = totalPrice / numberOfMembers
+            // Update amount for each member
+            for index in members.indices {
+                members[index].amount = memberAmount
+            }
+        }
+    }
+    
+    func deleteMember(at offsets: IndexSet) {
+        members.remove(atOffsets: offsets)
+    }
+    
+    func deletePaymentOption(at offsets: IndexSet) {
+        paymentOptions.remove(atOffsets: offsets)
+    }
+}
+
+struct Member {
+    var name: String
+    var amount: Double
+}
+
+struct PaymentOption {
+    var accountNumber: String
+    var bankName: String
+    var owner: String
+}
+
+struct AddMemberSheet: View {
+    @Binding var newMemberName: String
+    var onSave: () -> Void
+    var onCancel: () -> Void
+    
+    var body: some View {
+        VStack {
+            Text("New Member")
+                .font(.headline)
+                .padding()
+            
+            TextField("Enter member name", text: $newMemberName)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            HStack {
+                Button("Cancel") {
+                    onCancel()
+                }
+                .foregroundColor(.blue)
+                Spacer()
+                Button("Save") {
+                    onSave()
+                }
+                .foregroundColor(.blue)
+            }
+            .padding()
+        }
+        .padding()
+    }
+}
+
+struct AddPaymentSheet: View {
+    @Binding var newPaymentAccount: String
+    @Binding var newPaymentBank: String
+    @Binding var newPaymentOwner: String
+    var onSave: () -> Void
+    var onCancel: () -> Void
+    
+    var body: some View {
+        VStack {
+            Text("New Payment Option")
+                .font(.headline)
+                .padding()
+            
+            TextField("Account Number", text: $newPaymentAccount)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            TextField("Bank Name", text: $newPaymentBank)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            TextField("Owner", text: $newPaymentOwner)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            HStack {
+                Button("Cancel") {
+                    onCancel()
+                }
+                .foregroundColor(.blue)
+                Spacer()
+                Button("Save") {
+                    onSave()
+                }
+                .foregroundColor(.blue)
+            }
+            .padding()
+        }
+        .padding()
+    }
+}
+
+struct AddPatunganView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddPatunganView()
+    }
+}
